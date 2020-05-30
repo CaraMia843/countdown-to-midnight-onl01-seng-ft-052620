2a1293399c03f0eb093a4be5feb683e86d824dda
Cara Zellner <carajeanine@gmail.com>
2020-05-30 21:33:02 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -2,6 +2,15 @@ def countdown
   number = 10
   while number > 0
     puts "#{number} SECONDS!"
+    number -= 1
+  end
+ puts "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep
+  number = 10
+  while number > 0
+    puts "#{number} SECONDS!"
     sleep(1) 
     number -= 1
   end
